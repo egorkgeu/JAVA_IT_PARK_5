548af1c3d066ab5598372c1e6776cf07378f6771
student8 <student8@itpark.ru>
2018-03-29 20:39:56 +0300

    fix gitignore

---
+++ ClassWorks/Program/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a[] = {5, 6, 1, 2, 2};
+       
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+
+        System.out.println();
+
+    }
+}
