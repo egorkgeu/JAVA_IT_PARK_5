b8b98d8f989604e9b383ab7c8b4daf958ba4a5ee
Egorkgeu <ThreeDaysGraceEgor@gmail.com>
2018-03-30 01:24:58 +0300

    Homework - Task2

--- HomeWorks/Task 2/Program.java
+++ HomeWorks/Task 2/Program.java
@@ -44,21 +44,11 @@ class Program {
 							int Sum = 0;
 							System.out.print("Введите любое целое число: ");
 							int n = scanner.nextInt();
-							System.out.print("Введите диапазон значений a и b (число a не может быть больше числа b): ");
-							System.out.println();
-							System.out.print("Введите число а: ");
-							int a = scanner.nextInt();
-							System.out.print("Введите число b: ");
-							int b = scanner.nextInt();
-							    if(a > b){
-								System.out.println("Error. Число a не может быть больше числа b");
-								return;
-							    }
-							        for (int i = 1; i <= n; i++) {
-								    int random_number = a + (int) (Math.random() * b);
-							        System.out.println(i + " - Случайное число: " + random_number);
-								    Sum+=random_number;
-							        }
+							for (int i = 1; i <= n; i++) {
+							    System.out.print(i + " - случайное число: ");
+							    int a = scanner.nextInt();
+							    Sum+=a;
+							}
 							System.out.println("Сумма случайных чисел: " + Sum);
 							break;
 						}
